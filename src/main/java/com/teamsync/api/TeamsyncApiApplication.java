f8d5fd17e068005e7de71034cf012738a99a55c1
mdmx <143160694+Madmax-xhyyy@users.noreply.github.com>
2026-07-17 20:06:35 +0800

    Initial project setup

---
+++ src/main/java/com/teamsync/api/TeamsyncApiApplication.java
@@ -0,0 +1,13 @@
+package com.teamsync.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamsyncApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamsyncApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teamsync/api/TeamsyncApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamsync.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamsyncApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
